69c785edca4d8313f210b7646870628b5288d465
Nguyễn Long <hoanglong.lumi404@gmail.com>
2024-08-06 08:27:50 +0000

    pinctrl: samsung: add initial support for exynos8895
    Change-Id: I8a9432c8f10b16a320b291d90d544705771738d1

--- drivers/pinctrl/samsung/pinctrl-samsung.h
+++ drivers/pinctrl/samsung/pinctrl-samsung.h
@@ -342,6 +342,7 @@ extern const struct samsung_pinctrl_of_match_data s3c2416_of_data;
 extern const struct samsung_pinctrl_of_match_data s3c2440_of_data;
 extern const struct samsung_pinctrl_of_match_data s3c2450_of_data;
 extern const struct samsung_pinctrl_of_match_data s5pv210_of_data;
+extern const struct samsung_pinctrl_of_match_data exynos8895_of_data;
 extern const struct samsung_pinctrl_of_match_data exynos9610_of_data;
 extern const struct samsung_pinctrl_of_match_data exynos9630_of_data;
 extern const struct samsung_pinctrl_of_match_data exynos3830_of_data;
