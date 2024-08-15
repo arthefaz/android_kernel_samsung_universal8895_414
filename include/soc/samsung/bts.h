12a9031d7066728437bfc191cf69e63a049c9105
Nguyễn Long <hoanglong.lumi404@gmail.com>
2024-08-15 13:09:39 +0000

    drivers: bts: Add support for exynos8895
    Change-Id: I1e57f841cd901ccc0c14294bf325d43a3eb7d26f

--- include/soc/samsung/bts.h
+++ include/soc/samsung/bts.h
@@ -93,6 +93,92 @@ void bts_update_scen(enum bts_scen_type type, unsigned int val);
 void bts_update_bw(enum bts_bw_type type, struct bts_bw bw);
 unsigned int bts_calc_bw(enum bts_bw_type type, void *data);
 
+#elif defined(CONFIG_EXYNOS8895_BTS)
+#define BUS_WIDTH		16
+#define DISP_UTIL		75
+
+enum bts_scen_type {
+	BS_DEFAULT,
+	BS_MIF_CHANGE,
+	BS_MFC_UHD,
+	BS_MFC_UHD_10BIT,
+	BS_G3D_PEFORMANCE,
+	BS_DP_DEFAULT,
+	BS_CAMERA_DEFAULT,
+	BS_MAX,
+};
+
+enum bts_bw_type {
+	/* RT */
+	BTS_BW_DECON0,
+	BTS_BW_DECON1,
+	BTS_BW_DECON2,
+	BTS_BW_CAMERA,
+	BTS_BW_AUDIO,
+	BTS_BW_CP,
+	/* non-RT */
+	BTS_BW_G2D,
+	BTS_BW_MFC,
+	BTS_BW_MCSL,
+	BTS_BW_IVA,
+	BTS_BW_DSP,
+	BTS_BW_MAX,
+	BTS_BW_RT = BTS_BW_G2D,
+};
+
+enum bts_dpp_type {
+	BTS_DPP0,
+	BTS_DPP1,
+	BTS_DPP2,
+	BTS_DPP3,
+	BTS_DPP4,
+	BTS_DPP5,
+	BTS_DPP_MAX,
+};
+
+enum bts_dpu_type {
+	BTS_DPU0,
+	BTS_DPU1,
+	BTS_DPU2,
+	BTS_DPU_MAX,
+};
+
+struct bts_layer_position {
+	unsigned int x1;
+	/* x2 = x1 + width */
+	unsigned int x2;
+	unsigned int y1;
+	/* y2 = y1 + height */
+	unsigned int y2;
+};
+
+struct bts_dpp_info {
+	bool used;
+	unsigned int bpp;
+	unsigned int src_h;
+	unsigned int src_w;
+	struct bts_layer_position dst;
+	unsigned int bw;
+};
+
+struct bts_decon_info {
+	struct bts_dpp_info dpp[BTS_DPP_MAX];
+	/* Khz */
+	unsigned int vclk;
+	unsigned int lcd_w;
+	unsigned int lcd_h;
+};
+
+struct bts_bw {
+	unsigned int peak;
+	unsigned int read;
+	unsigned int write;
+};
+
+void bts_update_scen(enum bts_scen_type type, unsigned int val);
+/* bandwidth (KB/s) */
+void bts_update_bw(enum bts_bw_type type, struct bts_bw bw);
+unsigned int bts_calc_bw(enum bts_bw_type type, void *data);
 #else
 #define bts_update_scen(a, b) do {} while(0)
 #define bts_update_bw(a, b) do {} while(0)
